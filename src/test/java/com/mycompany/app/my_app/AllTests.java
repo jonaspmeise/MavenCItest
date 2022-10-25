1255b71def59f739b5339cbd402c2ce6321550a7
jonaspmeise <62212517+jonaspmeise@users.noreply.github.com>
2022-10-25 10:52:05 +0200

    Added example Test cases

---
+++ src/test/java/com/mycompany/app/my_app/AllTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.app.my_app;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+        CalculatorTest.class})
+
+public class AllTests {
+
+}
